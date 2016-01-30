98688a9dd5f2c534e8908f8a463b7c2f1081ac84
Extra <extra84@gmail.com>
2016-01-30 23:35:06 +0100

    fix users

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ if [ ! -e /data/murmur.ini ]; then
 	echo Starting Initialization
 	cp /etc/murmur.tpl /data/murmur.ini
 	if [ ! -z "$SERVER_PASSWORD" ] ; then echo 'serverpassword='"$SERVER_PASSWORD" >> /data/murmur.ini ; fi
-	if [ ! -z "$MAX_USERS" ] ; then echo 'maxusers='"$MAX_USERS" >> /data/murmur.ini ; fi
+	if [ ! -z "$MAX_USERS" ] ; then echo 'users='"$MAX_USERS" >> /data/murmur.ini ; fi
 	if [ ! -z "$SERVER_TEXT" ] ; then echo 'welcometext='"$SERVER_TEXT" >> /data/murmur.ini ; fi
 	if [ ! -z "$REGISTER_NAME" ] ; then echo 'registerName='"$REGISTER_NAME" >> /data/murmur.ini ; fi
 	if [ ! -z "$BANDWIDTH" ] ; then echo 'bandwidth='"$BANDWIDTH" >> /data/murmur.ini ; fi
